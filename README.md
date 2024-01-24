c1ab851b3fc482f9eaf6b2dcc2aaf0d0fe7f3442
Icaro Asas Virtuais <icaro.capobianco@gmail.com>
2024-01-24 09:10:32 -0300

    + links

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # auth.asasvirtuais.com
 
 NextJS + Auth0 sample project
+
+[auth.asasvirtuais.com](https://auth.asasvirtuais.com/)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Heading } from '@chakra-ui/react'
+import { comfortaa } from '@/next/fonts'
+import { Heading, Link } from '@chakra-ui/react'
 import Center from 'asasvirtuais/src/components/screen-center'
 
 export default function Home() {
   return (
     <Center>
       <Heading>asas virtuais auth</Heading>
+      <Heading fontSize='2xl' style={comfortaa.style}>Next + Auth0 example</Heading>
+      <Link href='https://github.com/icarocapobianco/auth.asasvirtuais.com'>github.com/icarocapobianco/auth.asasvirtuais.com</Link>
     </Center>
   )
 }
